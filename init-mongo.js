966779834e8254b03125e3204476c3ca3eba93ab
Louis Charavner <louis.charavner@viseo.com>
2020-12-04 14:15:18 +0100

    remove application and setup windows configuration and setup local save of MongoDB data

--- init-mongo.js
+++ init-mongo.js
@@ -1,2 +1,13 @@
-use process.env.DATABASE_NAME
-db.initDb.insertOne({"name": "hello, world !"})
+db.createUser(
+	{
+		user: '$DATABASE_USERNAME',
+		pwd: "$DATABASE_PASSWORD",
+		roles : [
+			{
+				role : "readWrite",
+				db: "$DATABASE_NAME"
+			}
+		]
+	}
+)
+db.initCollection.insert({name: "Hello, world !"})
