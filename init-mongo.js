19ee1aec1cf7b705de7cc60bdd12b533e222ca87
Charavner Louis <Distil62@users.noreply.github.com>
2020-12-07 12:22:07 +0100

    Update init-mongo.js

--- init-mongo.js
+++ init-mongo.js
@@ -1,13 +1,1 @@
-db.createUser(
-	{
-		user: '$DATABASE_USERNAME',
-		pwd: "$DATABASE_PASSWORD",
-		roles : [
-			{
-				role : "readWrite",
-				db: "$DATABASE_NAME"
-			}
-		]
-	}
-)
 db.initCollection.insert({name: "Hello, world !"})
