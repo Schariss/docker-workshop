25c55b9e4d0617d665a154a79464d9440fd02141
Distil62 <lcharav@factovia.fr>
2021-08-09 15:57:49 +0200

    implement add application modifications

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-# Dans ce fichier vous devez définir les instructions pour la construction de votre application
-# Notre application est en C# et utilise le framework .NET core 5
-# Liens utiles :
-# https://hub.docker.com/_/microsoft-dotnet-sdk
-# https://docs.docker.com/engine/reference/builder/#from
+# docker build . -t dotnet-dev
+# docker run -it -v "$(pwd):/app" dotnet-dev
+FROM mcr.microsoft.com/dotnet/sdk:5.0
+
+WORKDIR /app
+
+COPY . .
+
+CMD ["bash"]
---
+++ myApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using mongo_docker_sample.Models;
+using mongo_docker_sample.Services;
+
+namespace mongo_docker_sample
+{
+    class Program
+    {
+
+        static string DatabaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+        static string DatabaseUser = Environment.GetEnvironmentVariable("DATABASE_USERNAME");
+        static string DatabasePwd = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
+        static string DatabaseHost = Environment.GetEnvironmentVariable("DATABASE_HOST");
+
+        static void Main(string[] args)
+        {
+            string action = args.Length > 0
+                ? args[0]
+                : "";
+            string MongoDBConnectionString = $"mongodb://{DatabaseUser}:{DatabasePwd}@{DatabaseHost}:27017/?authSource=admin";
+
+            var Service = new BookService(MongoDBConnectionString, DatabaseName);
+
+            switch (action)
+            {
+                case "add":
+                    string bookName = args.Length > 1
+                     ? args[1]
+                     : "Livre sans nom";
+
+                     decimal bookPrice = args.Length > 2
+                     ? decimal.Parse(args[2])
+                     : 1m;
+                    Book book = new Book(bookName, bookPrice);
+                    Service.Create(book);
+                    var booksAdd = Service.Get();
+                    booksAdd.ForEach(Console.WriteLine);
+                    break;
+                default:
+                    var books = Service.Get();
+                    books.ForEach(Console.WriteLine);
+                    break;
+
+            }
+        }
+    }
+}
